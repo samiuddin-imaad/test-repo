9b65a40413c0cb8c720201611e8718b1c20a04f5
samiuddin-imaad <30466496+samiuddin-imaad@users.noreply.github.com>
2024-11-01 10:50:28 +0530

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -11,7 +11,7 @@ public class Testing {
 	 */
 	public static void main(String[] args) {
 		HtmlUnitDriver driver=new HtmlUnitDriver(BrowserVersion.FIREFOX_38);
-		driver.get("http://172.31.15.232:8080/qadeploy/");
+		driver.get("http://172.31.3.179:8080/qaenvironment/");
 		System.out.println(driver.getCurrentUrl());
 		String expmsg="Hello, World!";
 		String actmsg=driver.findElement(By.xpath("html/body")).getText();
